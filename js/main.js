e489f4fb01ac06fc12360c61d41f4c4a004af0e0
techiefer <adedokun.f@yahoo.com>
2020-03-12 17:27:35 +0100

    blah blah: did some work though � �

--- js/main.js
+++ js/main.js
@@ -1,30 +1,68 @@
-const weightInput = document.getElementsByClassName(".weight-form");
-const heightInput = document.getElementsByClassName('.height-form');
-const submit = document.getElementsByClassName('.form-button');
+const weightInput = document.getElementById("weight-form");
+const heightInput = document.getElementById("height-form");
+const outputTable = document.getElementById("output-area");
 
-function inputs(e) {
-    e.preventDefault();
-    const weightInputText = document.querySelector('[name=weight').value;
-    const heightInputText = document.querySelector('[name=height').value
-    if (weightInputText == null || undefined) {
-        return `weightInputText.createElement(p).appendChild(.weightInput)`;
+const submit = document.querySelector('#form-button');
+const mainForm = document.querySelector('#form-content');
+
+//getting values from the form field
+function Values() {
+    let name = document.getElementById('name-form').value;
+    let weightValue = weightInput.value;
+    let heightValue = heightInput.value;
+    
+    //checks for the length of the value
+   
+    //the instantiated object
+    const table = {
+        name: [],
+        weight: [],
+        height: [],
+        added: false
     }
-    //console.log(weightInputText);
-    //console.log(heightInputText);
+    //passes the values gotten into the object declaration
+    table.name.push(name);
+    table.weight.push(weightValue);
+    table.height.push(heightValue);
+
+    //bidning the object to the local Storage
+    localStore.push(table);
+
+    //to create the table
+    tableConstruct(localStore, outputTable);
+
+    localStorage.setItem("values",JSON.stringify(table));
+
 }
 
-function calculation() {
-    const result = parseInt()
+let localStore = localStorage.getItem("table") || []
+
+//responsible for creation of the table 
+function tableConstruct(inputs = [], outputs) {
+   outputs.innerHTML = inputs
+        .map((input) => {
+            return `
+        <div class="heading-table">
+            <th class="weight-table">Weight</th>
+            <th class="height-table">Height</th>
+            <tr>
+                <td>${input.weight}</td>
+                <td>${input.height}</td>
+            </tr>
+
+            <span data-text="${input.name}" class="deletion">&#x2715</span>
+        </div>
+        `;
+        })
+    .join('');
 }
-let output = (weightInput = [], heightInput = []) => {
-    return ``
-}
+
 
 function toggleSubmit(e) {
-    submit.addEventListener('submit', (e) => {
-        e.preventDefault();
-        inputs;
-    });
+    e.preventDefault()
+    console.log(Values());
 }
-
-function createTableOfOutputs() {}
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('System DOM has been set')
+})
+mainForm.addEventListener("submit", toggleSubmit);
